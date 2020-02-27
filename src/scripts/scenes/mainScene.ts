be2bb9e8a20af0b8373f5d8b75d709c51567b8f9
Adam <adamcj@udel.edu>
2020-02-27 16:28:17 -0500

    added background image (video 3 @ 1:05)

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,14 +2,23 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
+    this.background = this.add.image(0,0,"morty_bg");
+    this.background.setOrigin(0,0);
+
     this.exampleObject = new ExampleObject(this, 0, 0);
-    this.add.text(20,20, "Playing game", {font: "25px Arial", fill: "yellow"});
+    
+    this.add.text(20,20, "Playing game", { 
+      font: "25px Arial", 
+      fill: "yellow"
+    });
+  
   }
 
   update() {
