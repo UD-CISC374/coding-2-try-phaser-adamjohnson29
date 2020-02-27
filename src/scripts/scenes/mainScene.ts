f19c43440332ee7ccde36e5262ef2477ec42b0ab
Adam <adamcj@udel.edu>
2020-02-27 16:11:15 -0500

    playing game text added (video 2 @ 3:47)

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -9,6 +9,7 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.exampleObject = new ExampleObject(this, 0, 0);
+    this.add.text(20,20, "Playing game", {font: "25px Arial", fill: "yellow"});
   }
 
   update() {
